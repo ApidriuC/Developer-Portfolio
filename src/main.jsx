011cc74d6f59e180567112275f16e203e872beaf
Andres Felipe Londoño Campos <alondono@syc.com.co>
2024-11-22 16:48:33 -0500

    Se actualizan referencias

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import './locales/i18n'
-import App from './App.jsx'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './styles/main.css'
 import './styles/tailwindInput.css'
 import './styles/tailwindOutput.css'
+
+import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
