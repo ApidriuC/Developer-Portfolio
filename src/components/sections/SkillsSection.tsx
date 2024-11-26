199b27fe787314c92ead3f38df265552bef34cd9
Andres Felipe Londoño Campos <alondono@syc.com.co>
2024-11-26 11:42:56 -0500

    Se realiza optimización de función estática,

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import React, { useState, useCallback } from 'react';
 import { EducationBoxProps, PopupProps, SkillBoxProps, ContainerSkillProps } from 'types/props';
 import {
     sqlImage,
